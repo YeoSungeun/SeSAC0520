90d3ad780ca9957384b6eeefde985b452eb10356
YeoSungeun <dutjddms@naver.com>
2024-05-20 11:54:17 +0900

    @IBOutle vs @IBColllection

--- SeSAC0520/ViewController.swift
+++ SeSAC0520/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var labelList: [UILabel]!
+    
     @IBOutlet var oneButton: UIButton!
     @IBOutlet var twoButton: UIButton!
     @IBOutlet var threeButton: UIButton!
@@ -41,6 +44,7 @@ class ViewController: UIViewController {
     // 1. 어떤 버튼을 클릭했는지 어떻게 아나요? -> 매개변수 존재!
     // -> currentTitle (옵셔널 조심, 버전 조심)
     // -> tag
+    // 2. 아울렛 뭐 어떻게 못할까?? => OutleCollection
     @IBAction func oneButtonClicked(_ sender: UIButton) {
         
         print("tag: \(sender.tag)") // 현재 사용하는 타이틀이 뭔지!
@@ -52,12 +56,12 @@ class ViewController: UIViewController {
 //        } else {
 //            count[2] += 1
 //        }
-        
         count[sender.tag] += 1
         
-        oneLabel.text = "\(count[0])번 클릭"
-        twoLabel.text = "\(count[1])번 클릭"
-        threeLabel.text = "\(count[2])번 클릭"
+//        oneLabel.text = "\(count[0])번 클릭"
+//        twoLabel.text = "\(count[1])번 클릭"
+//        threeLabel.text = "\(count[2])번 클릭"
+        labelList[sender.tag].text = "\(count[sender.tag])번 클릭"
        
     }
     
